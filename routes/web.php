2f952f7af75eebb1812bb88fd4e0953ed5331288
Jose Sánchez Mateo <Sanchez069@gmail.com>
2022-02-20 17:42:26 +0100

    continue with seeder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
